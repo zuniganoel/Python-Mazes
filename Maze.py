3e138b3c8fed130e1dbc7e1515b15a7d85e8a4da
Enrique Zuniga <zunigaenrique94@gmail.com>
2020-04-15 17:52:30 -0700

    Refactored Code
    Maze contains the maze class that will implement all the algorithms

---
+++ Maze.py
@@ -0,0 +1,11 @@
+class Maze():
+    def __init__(self):
+        self.walls = {}
+        self.start = None
+        self.end = None
+
+    def generate(self):
+        print('Generating random maze')
+
+    def solve(self, algorithm):
+        print(f'Solving through {algorithm}')
